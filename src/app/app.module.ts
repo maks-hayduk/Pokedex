fb213399585b49c2b1841dab8f7c9790ff0e3661
maxon-h <maks.hayduk@gmail.com>
2019-05-13 16:49:00 +0300

    Created main/service/pipe components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { TypeFilterPipe } from './shared/pipes/type-filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    TypeFilterPipe
   ],
   imports: [
     BrowserModule,
